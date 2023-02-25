c434f916f7374e283eb1c0b884c102c200a8f141
officialpradip <study.pradipthapa@gmail.com>
2023-02-25 08:39:42 +0545

    spreed operator

--- beforeReact/app.js
+++ beforeReact/app.js
@@ -15,8 +15,28 @@ demo("ram")
 let demoArrow=(name="Pradip")=>name + " "+"Thapa";
 console.log(demoArrow("shyam"))
 
-//rest parameter
+//rest parameter ...
 let rest=(...arg)=>{
     console.log(arg)
 }
 rest("Hello","Hi")
+
+//spreed operator ...
+let fruits=["apple","mango"]
+console.log(fruits)
+fruits[0]="Orange"
+console.log(...fruits)
+
+//spreed operator in objects
+
+let person={
+    "name":"Shyam",
+    "cast":"Roy"
+
+}
+let movie={
+    "name":"Shyam SignhRoy"
+}
+let full={...person,...movie}
+console.log(full)
+console.log(person.cast)
