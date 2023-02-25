42f4466bdc4ff4f256f277f8874a000f81480072
officialpradip <study.pradipthapa@gmail.com>
2023-02-25 08:13:01 +0545

    before react

---
+++ beforeReact/app.js
@@ -0,0 +1,16 @@
+//es6
+//1. Vatiable declaration
+const a=1;
+console.log(a)
+
+let b=20
+console.log(b=30)
+
+//before es6
+function demo(name){
+    console.log("this is::",name)
+}
+demo("ram")
+//es6
+let demoArrow=(name="Pradip")=>name + " "+"Thapa";
+console.log(demoArrow("shyam"))
