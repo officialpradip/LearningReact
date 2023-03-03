f8bbb417bc3f6dbc0007afdb7fbe16caec671e2d
officialpradip <study.pradipthapa@gmail.com>
2023-03-03 09:44:28 +0545

    added components

---
+++ my-app/src/components/Promo.js
@@ -0,0 +1,13 @@
+function Promo(){
+    return(
+        <div className="promo-section">
+            <div>
+                <h1>Don't Miss This Deal!!</h1>
+            </div>
+            <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+
+        </div>
+    );
+};
+
+export default Promo;
