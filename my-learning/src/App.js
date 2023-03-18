5f2757ab0040c20904b5934ca39b03f1da68f27d
officialpradip <study.pradipthapa@gmail.com>
2023-03-18 13:34:48 +0545

    added files

---
+++ my-learning/src/App.js
@@ -0,0 +1,9 @@
+import './App.css'
+function App(){
+    return(
+        <div className="app-container"><h1> My first Components</h1>
+        <p>This is p tag</p></div>
+        
+    )
+}
+export default App;
--- my-learning/src/index.js
+++ my-learning/src/index.js
@@ -1,3 +1,6 @@
 import ReactDom from 'react-dom'
 import './index.css'
+
+import App from './App'
 ReactDom.render(<h1>Hello</h1>,document.getElementById('root'))
+ReactDom.render(<App/>,document.getElementById('root'))
