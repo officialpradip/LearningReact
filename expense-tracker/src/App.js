8f1f6113e50832a068bb268e318f04e413ce0093
officialpradip <study.pradipthapa@gmail.com>
2023-03-19 11:55:46 +0545

    component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,13 +2,34 @@
 import './App.css';
 import ExpensesItems from './components/expensesItems';
 function App() {
-  let expenseDate= new Date(2023,3,19);
-  let expenseTitle="Insurance";
-  let expensePrice=300;
+  let expense=[{
+    id:'e1',
+    title:'Health Insurance',
+    price:100,
+    date:new Date(2023,3,19)
+
+  },
+  {
+    id:'e3',
+    title:'Home Insurance',
+    price:200,
+    date:new Date(2023,3,19)
+
+  },
+  {
+    id:'e3',
+    title:'Insurance',
+    price:500,
+    date:new Date(2023,3,19)
+
+  }
+]
   return (
     <div>
       <h2>Let's Started</h2>
-       <ExpensesItems date={expenseDate} title={expenseTitle} price={expensePrice}></ExpensesItems>
+       <ExpensesItems date={expense[0].Date} title={expense[0].title} price={expense[0].price}></ExpensesItems>
+       <ExpensesItems date={expense[1].Date} title={expense[1].title} price={expense[1].price}></ExpensesItems>
+       <ExpensesItems date={expense[2].Date} title={expense[2].title} price={expense[2].price}></ExpensesItems>
     </div>
    
   );
