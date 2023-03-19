c89c7b249697b4b3b9cba0c885f78206c44c7732
officialpradip <study.pradipthapa@gmail.com>
2023-03-19 09:24:46 +0545

    props

--- expense-tracker/src/components/expensesItems.js
+++ expense-tracker/src/components/expensesItems.js
@@ -1,11 +1,12 @@
 import './expensesItems.css'
 
 function ExpensesItems(){
+    let title="Car Insurance"
     return(
         <div className="expense-item">
             <div>March 12th 2023</div>
             <div className="expense-item-desc">
-                <h2>Car Insurance</h2>
+                <h2>{title}</h2>
                 <div className="expense-item-price">$200</div>
             </div>
         </div>
