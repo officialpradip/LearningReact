6596198e263be95535eee49c1ffd6b9da571454b
officialpradip <study.pradipthapa@gmail.com>
2023-03-19 11:44:54 +0545

    date

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import ExpensesItems from './components/expensesItems';
 function App() {
+  let expenseDate= new Date(2023,3,19);
+  let expenseTitle="Insurance";
+  let expensePrice=300;
   return (
     <div>
       <h2>Let's Started</h2>
-       <ExpensesItems></ExpensesItems>
+       <ExpensesItems date={expenseDate} title={expenseTitle} price={expensePrice}></ExpensesItems>
     </div>
    
   );
--- expense-tracker/src/components/expensesItems.js
+++ expense-tracker/src/components/expensesItems.js
@@ -1,13 +1,20 @@
 import './expensesItems.css'
 
-function ExpensesItems(){
-    let title="Car Insurance"
+function ExpensesItems(props){
+    const year=props.date.getFullYear();
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.toLocaleString('en-US',{day:'2-digit'});;
+
     return(
         <div className="expense-item">
-            <div>March 12th 2023</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item-desc">
-                <h2>{title}</h2>
-                <div className="expense-item-price">$200</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item-price">${props.price}</div>
             </div>
         </div>
     )
