890c8a3ceea1735853ad4447eca128088e9baec0
officialpradip <study.pradipthapa@gmail.com>
2023-03-20 11:37:16 +0545

    code refactor

--- expense-tracker/src/components/expensedate.js
+++ expense-tracker/src/components/expensedate.js
@@ -4,10 +4,10 @@ function ExpenseDate(props){
     const month=props.date.toLocaleString('en-US',{month:'long'});
     const day=props.date.toLocaleString('en-US',{day:'2-digit'});
     return(
-        <div className=".expense-date">
-                <div className=".expense-date-month">{month}</div>
-                <div className=".expense-date-year">{year}</div>
-                <div className=".expense-date-day">{day}</div>
+        <div className="expense-date">
+                <div className="expense-date-month">{month}</div>
+                <div className="expense-date-year">{year}</div>
+                <div className="expense-date-day">{day}</div>
             </div>
     )
 }
--- expense-tracker/src/components/expenses.js
+++ expense-tracker/src/components/expenses.js
@@ -3,9 +3,9 @@ import ExpensesItems from './expensesItems';
 function Expenses(props){
     return(
         <div className="expenses">
-            <ExpensesItems date={props.items[0].date} title={props.items[0].title} price={props.items[0].price}></ExpensesItems>
-       <ExpensesItems date={props.items[1].date} title={props.items[1].title} price={props.items[1].price}></ExpensesItems>
-       <ExpensesItems date={props.items[2].date} title={props.items[2].title} price={props.items[2].price}></ExpensesItems>
+       <ExpensesItems date={props.items[0].date} title={props.items[0].title} price={props.items[0].price}/>
+       <ExpensesItems date={props.items[1].date} title={props.items[1].title} price={props.items[1].price}/>
+       <ExpensesItems date={props.items[2].date} title={props.items[2].title} price={props.items[2].price}/>
         </div>
 
     )
