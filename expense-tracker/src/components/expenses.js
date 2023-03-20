71f5077097851d494641b8866968f65f522ecef8
officialpradip <study.pradipthapa@gmail.com>
2023-03-20 14:39:25 +0545

    nested components

--- expense-tracker/src/components/expenses.js
+++ expense-tracker/src/components/expenses.js
@@ -1,12 +1,13 @@
 import './expenses.css'
+import Card from './card';
 import ExpensesItems from './expensesItems';
 function Expenses(props){
     return(
-        <div className="expenses">
+        <Card className="expenses">
        <ExpensesItems date={props.items[0].date} title={props.items[0].title} price={props.items[0].price}/>
        <ExpensesItems date={props.items[1].date} title={props.items[1].title} price={props.items[1].price}/>
        <ExpensesItems date={props.items[2].date} title={props.items[2].title} price={props.items[2].price}/>
-        </div>
+        </Card>
 
     )
 }
--- expense-tracker/src/components/expensesItems.js
+++ expense-tracker/src/components/expensesItems.js
@@ -1,14 +1,15 @@
 import './expensesItems.css'
 import ExpenseDate from './expensedate';
+import Card from './card';
 function ExpensesItems(props){
     return(
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item-desc">
                 <h2>{props.title}</h2>
                 <div className="expense-item-price">${props.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 export default ExpensesItems;
