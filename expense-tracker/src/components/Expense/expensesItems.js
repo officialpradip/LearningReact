159db4d873694d1453f7475c9ac00e3c6873bc04
officialpradip <study.pradipthapa@gmail.com>
2023-03-23 07:39:56 +0545

    state

--- expense-tracker/src/components/Expense/expensesItems.js
+++ expense-tracker/src/components/Expense/expensesItems.js
@@ -1,15 +1,24 @@
 import ExpenseDate from './expensedate';
 import './expensesItems.css'
 import Card from '../UI/card';
-import React from 'react';
+import React,{ useState } from 'react';
+
+
 const ExpensesItems=(props)=>{
+    const [title,setTitle]=useState(props.title);
+
+    let demoClick=()=>{
+        setTitle("New Title");
+
+    }
     return(
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item-desc">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item-price">${props.price}</div>
             </div>
+            <button onClick={demoClick}>Change Title</button>
         </Card>
     )
 }
