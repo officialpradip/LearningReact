28f30ca0890fe5e1dbf71c36d08e022025dbe72f
officialpradip <study.pradipthapa@gmail.com>
2023-03-25 14:29:37 +0545

    multiple state

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,34 @@
 import './ExpenseForm.css';
-import React from 'react';
+import React,{useState} from 'react';
 const ExpenseForm=()=>{
+    const [title,setTitle]=useState('');
+    const [amount,setAmount]=useState('');
+    const [date,setDate]=useState('');
+    const titleHandler=(event)=>{
+        setTitle(event.target.value)
+    }
+    const amountHandler=(event)=>{
+        setAmount(event.target.value)
+    }
+    const dateHandler=(event)=>{
+        setDate(event.target.value)
+    }
     return(
         <form>
             <div className='new-expense-controls'>
                 <div className='new-expense-control'>
                     <label>Title</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={titleHandler}></input>
                 </div>
 
                 <div className='new-expense-control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"></input>
+                    <input type="number" min="0.01" step="0.01" onChange={amountHandler}></input>
                 </div>
 
                 <div className='new-expense-control'>
                     <label>Date</label>
-                    <input type="date"></input>
+                    <input type="date" onChange={dateHandler}></input>
                 </div>
 
             </div>
