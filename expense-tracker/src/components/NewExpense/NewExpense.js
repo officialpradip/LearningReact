4de766291cc769de3edfaed712128dcdfb037c6c
officialpradip <study.pradipthapa@gmail.com>
2023-03-25 13:48:45 +0545

    form start

--- expense-tracker/src/components/NewExpense/NewExpense.js
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import './NewExpense.css'
-
+import './NewExpense.css';
+import React from 'react';
 let NewExpense=()=>{
     return(
-        <div>
+        <div className='new-expense'>
+            <form></form>
 
         </div>
     )
