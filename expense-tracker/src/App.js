e617970132bd36721306d39a14ed70bfee93aca8
officialpradip <study.pradipthapa@gmail.com>
2023-03-28 11:49:10 +0545

    render data

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,38 +2,40 @@
 import './App.css';
 import Expenses from './components/Expense/expenses';
 import NewExpense from './components/NewExpense/NewExpense';
-import React from 'react'
-const App=()=> {
-  let expense=[{
-    id:'e1',
-    title:'Health Insurance',
-    price:100,
-    date:new Date(2023,3,19)
+import React,{useState} from 'react'
+let dummyexpense=[{
+  id:'e1',
+  title:'Health Insurance',
+  price:100,
+  date:new Date(2023,3,19)
 
-  },
-  {
-    id:'e3',
-    title:'Home Insurance',
-    price:200,
-    date:new Date(2023,3,19)
+},
+{
+  id:'e3',
+  title:'Home Insurance',
+  price:200,
+  date:new Date(2023,3,19)
 
-  },
-  {
-    id:'e3',
-    title:'Insurance',
-    price:500,
-    date:new Date(2023,3,19)
+},
+{
+  id:'e3',
+  title:'Insurance',
+  price:500,
+  date:new Date(2023,3,19)
 
-  }
+}
 ]
-  const assExpenseHandler=(expense)=>{
-    console.log(expense)
+const App=()=> {
+  const [expenses,setExpense]=useState(dummyexpense)
+  const assExpenseHandler = (expense)=>{
+    const updatedExpense=[expense,...expenses]
+    setExpense(updatedExpense)
   }
   return (
     <div>
       <h2>Let's Started</h2>
       <NewExpense assExpenseHandlerProps={assExpenseHandler}/>
-       <Expenses items={expense}/>
+       <Expenses items={expenses}/>
     </div>
    
   );
--- expense-tracker/src/components/Expense/expenses.js
+++ expense-tracker/src/components/Expense/expenses.js
@@ -7,9 +7,15 @@ import React from 'react';
 const Expenses=(props)=>{
     return(
         <Card className="expenses">
-       <ExpensesItems date={props.items[0].date} title={props.items[0].title} price={props.items[0].price}/>
-       <ExpensesItems date={props.items[1].date} title={props.items[1].title} price={props.items[1].price}/>
-       <ExpensesItems date={props.items[2].date} title={props.items[2].title} price={props.items[2].price}/>
+            {
+                props.items.map(
+                    e => (
+                    <ExpensesItems date={e.date} title={e.title} price={e.price}/>
+
+                ))
+            }
+       
+       
         </Card>
 
     )
